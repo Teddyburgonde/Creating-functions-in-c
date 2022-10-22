a70274ffc7bc11f2b627177d8e8813f7c6ac111f
Teddy Bandama <teddybandama@MacBook-Pro-de-Teddy.local>
2022-10-22 11:50:49 +0200

    covert to strupcase

---
+++ ft_strupcase.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+int	main(void)
+{
+	char	test[] = "salut";
+
+	printf("%s", ft_strupcase(test));
+}
