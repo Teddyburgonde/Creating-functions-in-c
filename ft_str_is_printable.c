6e102cb930b5afb8f49e0a3e8656dbffa4b468ff
Teddy Bandama <teddybandama@MacBook-Pro-de-Teddy.local>
2022-10-22 11:40:38 +0200

    Is it printable?

---
+++ ft_str_is_printable.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int	ft_str_is_printable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 32 && str[i] <= 126)
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	char	*test;
+
+	test = "	";
+	printf("%d", ft_str_is_printable(test));
+}
