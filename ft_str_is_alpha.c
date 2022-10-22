8c28ef25568709d74e38033874ea01806877b5a5
Teddy Bandama <teddybandama@MacBook-Pro-de-Teddy.local>
2022-10-22 11:32:17 +0200

    Is it a number or not?

---
+++ ft_str_is_alpha.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] >= 'A' && str[i] <= 'Z')
+			|| (str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	char	*test;
+
+	test = "salut";
+	printf("%d", ft_str_is_alpha(test));
+}
