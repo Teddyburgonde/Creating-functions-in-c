dfb787363de817b8ce61a9c47b7b3bbf02756c0a
Teddy Bandama <teddybandama@MacBook-Pro-de-Teddy.local>
2022-10-25 10:39:46 +0200

    calcul factorial

---
+++ ft_iterative_factorial.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+	int	i;
+
+	result = 1;
+	i = 1;
+	if (nb <= 0)
+		return (0);
+	while (i <= nb)
+	{
+		result *= i;
+		i++;
+	}
+	return (result);
+}
+
+int	main(void)
+{
+	printf("%d", ft_iterative_factorial(-4));
+}
