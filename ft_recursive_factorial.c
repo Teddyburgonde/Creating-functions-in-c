d46f5f66be42565e8e16a642cf12eee4ff04d59d
Teddy Bandama <teddybandama@MacBook-Pro-de-Teddy.local>
2022-10-27 14:11:22 +0200

    factorial of a number with recursive

---
+++ ft_recursive_factorial.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	else if (nb == 0)
+		return (1);
+	else
+		return (ft_recursive_factorial(nb - 1) * nb);
+}
+
+int	main(void)
+{
+	printf("%d", ft_recursive_factorial(5));
+}
